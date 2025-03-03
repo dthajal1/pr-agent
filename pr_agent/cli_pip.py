7f4c8dbb81a1c392bad2137dcb007e439842b590
diraj thajali <thajalidiraj@gmail.com>
2025-03-02 23:00:14 -0800

    use .env for sensitive vars

--- pr_agent/cli_pip.py
+++ pr_agent/cli_pip.py
@@ -1,14 +1,19 @@
 from pr_agent import cli
 from pr_agent.config_loader import get_settings
-
+import os
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
+    # print(os.getenv("GITHUB_TOKEN"))
+    # print(os.getenv("OPENAI_API_KEY"))
+
     # Fill in the following values
     provider = "github"  # GitHub provider
-    user_token = "..."  # GitHub user token
-    openai_key = "..."  # OpenAI key
-    pr_url = "..."  # PR URL, for example 'https://github.com/Codium-ai/pr-agent/pull/809'
-    command = "/review"  # Command to run (e.g. '/review', '/describe', '/ask="What is the purpose of this PR?"')
+    user_token = os.getenv("GITHUB_TOKEN")  # GitHub user token
+    openai_key = os.getenv("OPENAI_API_KEY")  # OpenAI key
+    pr_url = "https://github.com/dthajal1/docusaurus-doc-updater-X-turborepo/pull/1"  # PR URL, for example 'https://github.com/Codium-ai/pr-agent/pull/809'
+    command = "/ask 'What is the purpose of this PR?'"  # Command to run (e.g. '/help', '/review', '/describe', '/ask "What is the purpose of this PR?"')
 
     # Setting the configurations
     get_settings().set("CONFIG.git_provider", provider)
